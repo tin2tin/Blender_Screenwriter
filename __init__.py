fde85509b398ed73e3827712dbeb40297775d436
samytichadou <samytichadou@gmail.com>
2019-11-16 00:32:54 +0100

    multi_files_clean

--- __init__.py
+++ __init__.py
@@ -19,17 +19,31 @@ from bpy.props import IntProperty, BoolProperty, PointerProperty, StringProperty
 # load and reload submodules
 ##################################
 
-import importlib, inspect
-from . import developer_utils
-importlib.reload(developer_utils)
-modules = developer_utils.setup_addon_modules(__path__, __name__, "bpy" in locals())
-classes = []
-for module in modules:
-    for name, obj in inspect.getmembers(module):
-        if debug: print("registered --- " + name)
-        if inspect.isclass(obj) and name != "persistent":
-            classes.append(obj)
+# import importlib, inspect
+# from . import developer_utils
+# importlib.reload(developer_utils)
+# modules = developer_utils.setup_addon_modules(__path__, __name__, "bpy" in locals())
+# classes = []
+# for module in modules:
+#     for name, obj in inspect.getmembers(module):
+#         if debug: print("registered --- " + name)
+#         if inspect.isclass(obj) and name != "persistent":
+#             classes.append(obj)
 
+from .operators.dual_view import *
+from .operators.fountain_export import *
+from .operators.preview_fountain import *
+from .operators.scene_to_strip import *
+from .gui import *
+from .properties import *
+
+classes = (SCREENWRITER_PT_panel,
+            SCREENWRITER_OT_preview_fountain,
+            TEXT_OT_dual_view,
+            SCREENWRITER_OT_export,
+            TEXT_OT_scenes_to_strips,
+            TextReplaceProperties            
+            )
 
 # import specific
 ##################################
@@ -42,8 +56,15 @@ from .properties import TextReplaceProperties
 ##################################
 
 def register():
+    ### OPERATORS ###
+    from bpy.utils import register_class
+    for cls in classes :
+        register_class(cls)
+
+    ### MENU ###
     bpy.types.TEXT_MT_text.append(screenwriter_menu_export)
 
+    ### PROPERTIES ###
     bpy.types.Scene.last_character = IntProperty(default=0)
     bpy.types.Scene.last_line = StringProperty(default="")
     bpy.types.Scene.last_line_index = IntProperty(default=0)
@@ -51,8 +72,15 @@ def register():
 
 
 def unregister():
+    ### OPERATORS ###
+    from bpy.utils import register_class
+    for cls in classes :
+        unregister_class(cls)
+
+    ### MENU ###
     bpy.types.TEXT_MT_text.remove(screenwriter_menu_export)
 
+    ### PROPERTIES ###
     del bpy.types.Scene.last_character
     del bpy.types.Scene.last_line
     del bpy.types.Scene.last_line_index
