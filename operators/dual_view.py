28f5848fad15d370ed33bcd537747d394bbf24bb
samytichadou <samytichadou@gmail.com>
2019-11-16 16:38:37 +0100

    fixing dual view

--- operators/dual_view.py
+++ operators/dual_view.py
@@ -2,6 +2,8 @@ import bpy
 
 from pathlib import Path
 
+from ..debug_value import debug ### DEBUG
+
 
 class TEXT_OT_dual_view(bpy.types.Operator):
     '''Toggles screenplay preview'''
@@ -46,8 +48,11 @@ class TEXT_OT_dual_view(bpy.types.Operator):
 
         arealist = list(context.screen.areas)
 
+        if debug: print('debug --- start execution') ### DEBUG
+
         filename = "Preview.txt"
         if filename not in bpy.data.texts:
+            if debug: print('debug --- start creating the preview') ### DEBUG
             bpy.ops.scene.preview_fountain()
 
             fountain_script = bpy.context.area.spaces.active.text.as_string()
@@ -57,68 +62,54 @@ class TEXT_OT_dual_view(bpy.types.Operator):
                 return {"CANCELLED"}
 
         for area in context.screen.areas:
-            if area == thisarea:
-                continue
-            elif area.x == tgxvalue and area.y == thisarea.y:
-                otherarea = area
+            if debug: print('debug --- iterating through areas : ' + area.type) ### DEBUG
+            if area.type == "TEXT_EDITOR":
+                if area == thisarea:
+                    if debug: print('debug --- context area is this area') ### DEBUG
+                    continue
+                else:
+                    # check if this area is a preview one
+                    if area.spaces.active.text == bpy.data.texts['Preview.txt']:
+                        if debug: print('debug --- preview mode already exists') ### DEBUG
+                        return {"FINISHED"}
+
+        if debug: print('debug --- splitting context area and starting dual mode') ### DEBUG
+
+        areax = None
+
+        #split
+        window = context.window
+        region = context.region
+        screen = context.screen
+        main = context.area
+
+        main.type = "TEXT_EDITOR"
+        ctrlPanel = bpy.ops.screen.area_split(
+            direction="VERTICAL")  #, factor=0.7)
+
+        #settings for preview 2.
+        bpy.ops.screen.screen_full_area()
+        bpy.ops.screen.screen_full_area()
+        override = original
+        area = self.original_area
+        override['area'] = area
+        override['space_data'] = area.spaces.active
+        override['space_data'].text = bpy.data.texts['Preview.txt']
+        override['space_data'].show_region_ui = False
+        override['space_data'].show_region_header = False
+        override['space_data'].show_region_footer = False
+        override['space_data'].show_line_numbers = False
+        override['space_data'].show_syntax_highlight = False
+        override['space_data'].show_word_wrap = False
+
+        for area in context.screen.areas:
+            if area not in arealist:
+                areax = area
                 break
 
-        if otherarea:  #leave trim-mode
-
-            # The 2.81 API doesn't have an option for automatic joining.
-            bpy.ops.screen.area_join(
-                'INVOKE_DEFAULT',
-                cursor=(otherarea.x, otherarea.y + int(otherarea.height / 2)))
-
-            # normal settings
-            bpy.ops.screen.screen_full_area()
-            bpy.ops.screen.screen_full_area()
-            override = context.copy()
-            area = self.original_area
-            override['area'] = area
-            override['space_data'] = area.spaces.active
-
-            return {"FINISHED"}
-
-        else:  # enter dual-mode
-
-            areax = None
-
-            #split
-            window = context.window
-            region = context.region
-            screen = context.screen
-            main = context.area
-
-            main.type = "TEXT_EDITOR"
-            ctrlPanel = bpy.ops.screen.area_split(
-                direction="VERTICAL")  #, factor=0.7)
-
-            #settings for preview 2.
-            bpy.ops.screen.screen_full_area()
-            bpy.ops.screen.screen_full_area()
-            override = original
-            area = self.original_area
-            override['area'] = area
-            override['space_data'] = area.spaces.active
-            override['space_data'].text = bpy.data.texts['Preview.txt']
-            override['space_data'].show_region_ui = False
-            override['space_data'].show_region_header = False
-            override['space_data'].show_region_footer = False
-            override['space_data'].show_line_numbers = False
-            override['space_data'].show_syntax_highlight = False
-            override['space_data'].show_word_wrap = False
-
-            for area in context.screen.areas:
-                if area not in arealist:
-                    areax = area
-                    break
-
-            if areax:
-                areax.type = thistype
-                return {"FINISHED"}
-
-        return {"CANCELLED"}
-
+        if areax:
+            areax.type = thistype
+        
+        return {"FINISHED"}
 
 handler = None
