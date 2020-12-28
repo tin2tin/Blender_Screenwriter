7879b24ebf8bebc6e5d8a4be26e93d58dd613e1f
tin2tin <tintwotin@gmail.com>
2020-12-28 08:45:56 +0100

    Add obj assignment and use Gabriel Montagne's unfurl code for timings

--- gui.py
+++ gui.py
@@ -1,11 +1,18 @@
 import bpy
 
+from bpy.types import Panel, PropertyGroup, UIList, Operator, OperatorFileListElement
+#from bpy.props import StringProperty, CollectionProperty, IntProperty, PointerProperty, BoolProperty
+#from pathlib import Path
+#import bpy
+#from bpy_extras.io_utils import ImportHelper
+#from os import path
+
 class SCREENWRITER_PT_panel(bpy.types.Panel):
     """Preview fountain script as formatted screenplay"""
     bl_label = "Screenwriter"
     bl_space_type = 'TEXT_EDITOR'
     bl_region_type = 'UI'
-    bl_category = "Text"
+    bl_category = "Screenwriter"
 
     def draw(self, context):
         layout = self.layout
@@ -14,10 +21,12 @@ class SCREENWRITER_PT_panel(bpy.types.Panel):
         row = layout.row(align=True)
         row.operator("screenwriter.dual_view")
         row.operator("scene.preview_fountain", text="", icon="FILE_REFRESH")
+        
         repl = context.scene.text_replace
         layout.prop(repl, "enabled")
+
         layout.operator("screenwriter.insert_title_page")
-        layout.operator("screenwriter.insert_scene_numbers")
+        # layout.operator("screenwriter.insert_scene_numbers") #unfinished implementation
 
 
 class SCREENWRITER_PT_sequencer_panel(bpy.types.Panel):
@@ -27,11 +36,77 @@ class SCREENWRITER_PT_sequencer_panel(bpy.types.Panel):
     bl_options = {'DEFAULT_CLOSED'}
     bl_space_type = 'TEXT_EDITOR'
     bl_region_type = 'UI'
-    bl_category = "Text"
+    bl_category = "Screenwriter"
 
     def draw(self, context):
-        self.layout.operator("text.scenes_to_strips")
-        self.layout.operator("text.switch_to_scene")
+        layout = self.layout
+        scn = context.scene
+        props = scn.keywords_assigner
+        layout = layout.column(align=False)
+
+        row = layout.row(align=False)
+        row.prop(props, "new_keyword", text="Word")
+        row.operator("ops.rename_keyword", icon="COPY_ID", text="")
+        row.operator("ops.add_keyword", icon="ADD", text="")
+
+        row = layout.row()
+        row.template_list("OBJECT_UL_screenwriter_keywords", "", props, "keywords", props, "keyword_index", rows=5)
+
+        col = row.column()
+        col.operator("ops.sw_remove_keyword", text="", icon="REMOVE")
+        col.operator("ops.sw_move_keyword_up", text="", icon="TRIA_UP")
+        col.operator("ops.sw_move_keyword_down", text="", icon="TRIA_DOWN")
+
+        if 0 <= props.keyword_index < len(props.keywords):
+            #layout.label(text="Objects")
+            keyword = props.keywords[props.keyword_index]
+
+            row = layout.row()
+            row.template_list("OBJECT_UL_screenwriter_objects", "", keyword, "objects", keyword, "object_index",
+                              rows=7)
+
+            col = row.column()
+            col.operator("ops.sw_add_objects", text="", icon="ADD")
+            col.operator("ops.sw_remove_file", text="", icon="REMOVE")
+            col.operator("ops.sw_move_file_up", text="", icon="TRIA_UP")
+            col.operator("ops.sw_move_file_down", text="", icon="TRIA_DOWN")
+
+#            layout.separator()
+
+#            col = layout.row(align=True)
+#            col.operator("ops.sw_load_reload_objects", icon="FILE_REFRESH")
+#            col.operator("ops.sw_remove_keyword_objects", icon="REMOVE")
+#            col = layout.row(align=True)
+#            col.operator("ops.sw_save_keyword_objects", icon="WINDOW")
+#            col.operator("ops.sw_run_objects", icon="PLAY")
+            
+        #layout.operator("text.scenes_to_strips")
+
+        layout.operator("screenwriter.fountain_to_strips")
+        #layout.operator("screenwriter.strips_to_markers")
+        #layout.operator("screenwriter.clear_markers")
+        #layout.prop(context.scene, 'screenwriter_channel')
+        layout.separator()
+        layout.operator("text.switch_to_scene")
+
+# ---------------------------------------------------------------------------------------------
+# UI LISTS
+# ---------------------------------------------------------------------------------------------
+class OBJECT_UL_screenwriter_keywords(UIList):
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
+        row = layout.row(align=True)
+
+        row.label(text=item.name)
+        row.label(text="{} Object(s)".format(len(item.objects)))
+
+
+class OBJECT_UL_screenwriter_objects(UIList):
+    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
+        row = layout.row(align=True)
+
+        row.label(text=item.objectname)
+        row.label(text=item.objecttype.title())
+        # row.prop(item, "runnable", text="Visible")
 
 
 def screenwriter_menu_export(self, context):
