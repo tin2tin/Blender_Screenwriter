59d70b6083943c98fd505cf441bc46f9103c1df8
tin2tin <tintwotin@gmail.com>
2021-01-14 09:22:50 +0100

    Update fountain_export.py

--- operators/fountain_export.py
+++ operators/fountain_export.py
@@ -64,23 +64,26 @@ def screenplay_export(context, screenplay_filepath, opt_exp, open_browser):
     from io import StringIO
     import webbrowser
     s = StringIO(fountain_script)
-    screenplay = fountain.parse_lines(s)
+    screenplay = fountain.parse(s)
     output = StringIO()
-    if opt_exp == "HTML":
-        from screenplain.export.html import convert
-        convert(screenplay, output, bare=False)
-    if opt_exp == "FDX":
-        from screenplain.export.fdx import to_fdx
-        to_fdx(screenplay, output)
-    if opt_exp == "PDF":
-        from screenplain.export.pdf import to_pdf
-        to_pdf(screenplay, output)
-    sp_out = output.getvalue()
     filename, extension = os.path.splitext(screenplay_filepath)
     fileout_name = filename + "." + opt_exp.lower()
-    file = open(fileout_name, "w")
-    file.write(sp_out)
-    file.close()
+
+    if opt_exp == "HTML" or opt_exp == "FDX":
+        if opt_exp == "HTML":
+            from screenplain.export.html import convert
+            convert(screenplay, output, bare=False)
+        elif opt_exp == "FDX":
+            from screenplain.export.fdx import to_fdx
+            to_fdx(screenplay, output)
+        sp_out = output.getvalue()
+        file = open(fileout_name, "w")
+        file.write(sp_out)
+        file.close()
+    elif opt_exp == "PDF":
+        from screenplain.export.pdf import to_pdf
+        to_pdf(screenplay, fileout_name)
+
     if open_browser:
         if opt_exp == "HTML" or opt_exp == "PDF":
             webbrowser.open(fileout_name)
